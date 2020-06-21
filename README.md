cef8f69cd742d8d69c4a46251d0de4fd8dcbc0ac
Stephanie Piñero <stephanie.pinerog@gmail.com>
2020-06-21 11:33:40 -0400

    adding role to ec2 instance

--- README.md
+++ README.md
@@ -18,14 +18,28 @@
 
     ``` $ terraform apply ```
 
+---
+**NOTE**
+
+Luego de aplicar la creación de recursos se debe obtener la IP pública de la Instancia creada para poder acceder al API por el puerto 3000 (por ejemplo: 54.68.91.164:3000) 
+
+La IP tambien se puede obtener desde el dashboard de AWS en EC2
+
+---
+
 ## Recursos creados
 
 Los recursos son creados en la región us-west-2 (Oregon)
 
+- Role para la instancia EC2
+- Politica asociada al role
+- Profile para asociar el role a la instancia
 - Instancia EC2, tipo t2.micro
 - Tabla DynamoDB, subscriptions
 
-Luego de que los recursos son creados verificar que el SG de la instancia tenga permisos para acceder por el puerto 80 y 3000.
+Luego de que los recursos son creados verificar que el SG de la instancia EC2 tenga permisos para acceder por el puerto 80 y 3000.
+
+
 
 
 
--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
+resource "aws_iam_instance_profile" "gocloud_ec2_profile" {
+  name = "gocloud_ec2_profile"
+  role = aws_iam_role.gocloud_ec2_role.name
+}
+
 resource "aws_instance" "GoCloudAPI"{
     ami = "ami-0e34e7b9ca0ace12d"
     instance_type = "t2.micro"
     user_data = file("apache.sh")
+      iam_instance_profile = aws_iam_instance_profile.gocloud_ec2_profile.name
     #key_name = "gocloud"
 }
 
@@ -21,4 +27,45 @@ resource "aws_dynamodb_table" "subscriptions" {
     type = "S"
   }
 
+}
+
+resource "aws_iam_role_policy" "gocloud_ec2_policy" {
+  name = "gocloud_ec2_policy"
+  role = aws_iam_role.gocloud_ec2_role.id
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "dynamodb:UpdateItem",
+          "dynamodb:PutItem"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role" "gocloud_ec2_role" {
+  name = "gocloud_ec2_role"
+
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": "GoCloudRole"
+      }
+    ]
+  }
+  EOF
 }
